52aa100a38a7f928ab7c4fe0f8983a840db99d38
anapedra <anapedra.mil@gmail.com>
2024-04-11 15:10:45 -0300

    Projeto Padrões de projeto

---
+++ beckend/padroes-projeto-dio/src/main/java/com/anapedra/padroesprojetodio/PadroesProjetoDioApplication.java
@@ -0,0 +1,13 @@
+package com.anapedra.padroesprojetodio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjetoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoDioApplication.class, args);
+	}
+
+}
---
+++ beckend/padroes-projeto-dio/src/test/java/com/anapedra/padroesprojetodio/PadroesProjetoDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anapedra.padroesprojetodio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
