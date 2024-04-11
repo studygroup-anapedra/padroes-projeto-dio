bd063e3d415aa3a1caed92c949f1cf9a32bbd997
anapedra <anapedra.mil@gmail.com>
2024-04-11 16:29:07 -0300

    ClientRepository

--- beckend/padroes-projeto-dio/src/main/java/com/anapedra/padroesprojetodio/model/Client.java
+++ beckend/padroes-projeto-dio/src/main/java/com/anapedra/padroesprojetodio/model/Client.java
@@ -1,7 +1,7 @@
 package com.anapedra.padroesprojetodio.model;
 
 import jakarta.persistence.*;
-import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+
 
 @Entity
 public class Client {
